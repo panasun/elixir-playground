2088e776ef21269195936e24930af2390b51dece
Panasun Sunanta <panasunsunanta@gmail.com>
2023-05-25 13:41:00 +0700

    first commit

---
+++ graphql/lib/graphql_web/router.ex
@@ -0,0 +1,21 @@
+defmodule GraphqlWeb.Router do
+  use GraphqlWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", GraphqlWeb do
+    pipe_through :api
+  end
+
+  # Enable Swoosh mailbox preview in development
+  if Application.compile_env(:graphql, :dev_routes) do
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
