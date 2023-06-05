4546c341ed3aff32788ba24493a46f36c8feb86d
Panasun Sunanta <panasunsunanta@gmail.com>
2023-06-05 12:08:37 +0700

    fix: persist

---
+++ key_val/lib/key_val.ex
@@ -0,0 +1,18 @@
+defmodule KeyVal do
+  @moduledoc """
+  Documentation for `KeyVal`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KeyVal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
